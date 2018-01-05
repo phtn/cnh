9fbcb047f04877c7601bc83b3fe40332835433d7
phtn458 <phtn458@gmail.com>
2018-01-05 00:40:50 -0500

    added animated.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './animate.css';
 import './bootstrap.min.css'
 import bg from './assets/pattern.png'
 import FrontSlide from './components/FrontSlide'
@@ -8,18 +8,18 @@ import Footer from './components/Footer'
 const styles = {
   body: {backgroundColor: `transparent`},
   jumbo: {backgroundColor: 'transparent', marginTop: -35},
-  title: {color: '#100', fontSize: 64, textAlign: 'center'},
+  title: {color: '#100', fontSize: 64, textAlign: 'center', fontFamily: 'Roboto, sans-serif', fontWeight: 100},
   sub: {color: '#100', textAlign: 'center'},
 }
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={styles.body}>
+      <div className="App animated fadeIn" style={styles.body}>
         <div className="jumbotron" style={styles.jumbo}>
-          <h1 className="display-3" style={styles.title}>Clarion</h1>
-          <h5 className="display-5 text-muted" style={styles.sub}>NEW HOPE - LAMBERTVILLE</h5>
-            <hr className="my-4"/>
+          <h1 className="display-3 animated fadeInDown" style={styles.title}>Clarion</h1>
+          <h6 className="display-6 text-muted animated fadeInUp" style={styles.sub}>NEW HOPE - LAMBERTVILLE</h6>
+            <hr className="my-4 animated fadeInRight"/>
 
               <FrontSlide/>
 
