9315fb4a8cb305e1b9854f29056f1e2c7bdf6ba3
phtn458 <phtn458@gmail.com>
2018-01-05 00:18:16 -0500

    updated footer 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
 import './bootstrap.min.css'
 import bg from './assets/pattern.png'
 import FrontSlide from './components/FrontSlide'
+import Footer from './components/Footer'
 
 const styles = {
-  body: {backgroundColor: `transparent`, height: window.innerHeight},
-  jumbo: {backgroundColor: 'transparent', marginTop: -50},
-  title: {color: '#100', fontSize: 64}
+  body: {backgroundColor: `transparent`},
+  jumbo: {backgroundColor: 'transparent', marginTop: -35},
+  title: {color: '#100', fontSize: 64, textAlign: 'center'},
+  sub: {color: '#100', textAlign: 'center'},
 }
 
 class App extends Component {
@@ -16,15 +18,13 @@ class App extends Component {
       <div className="App" style={styles.body}>
         <div className="jumbotron" style={styles.jumbo}>
           <h1 className="display-3" style={styles.title}>Clarion</h1>
-          <h5 className="display-5 text-muted">NEW HOPE - LAMBERTVILLE</h5>
+          <h5 className="display-5 text-muted" style={styles.sub}>NEW HOPE - LAMBERTVILLE</h5>
             <hr className="my-4"/>
 
               <FrontSlide/>
 
           </div>
-        <header className="App-header">
-          <button type="button"  className="btn btn-outline-secondary">reservations</button>
-        </header>
+        <Footer/>
         <p className="App-intro">
 
         </p>
---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+const styles={
+  container: {
+    backgroundColor: 'rgba(0,0,0, 0.9)',
+    height: 75,
+    paddingTop: 15,
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    textAlign: 'center'
+  }
+}
+export default props => (
+  <div style={styles.container}>
+    <button type="button"  className="btn btn-outline-secondary">reservations</button>
+  </div>
+)
+
+// .App-header {
+//   background-color: #222;
+//   height: 150px;
+//   line-height: 100px;
+//   padding: 20px;
+//   color: white;
+//   position: fixed;
+//   width: 100%;
+//   bottom: 0;
+// }
