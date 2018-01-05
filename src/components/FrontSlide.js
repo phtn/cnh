feab2311264ba828c31e38467654b778e3379944
phtn458 <phtn458@gmail.com>
2018-01-05 00:55:23 -0500

    meh

--- src/components/FrontSlide.js
+++ src/components/FrontSlide.js
@@ -1,41 +1,35 @@
 import React from 'react'
-import REST1 from '../assets/erREST1.png'
+import POOL3 from '../assets/pool3.jpg'
 import REST2 from '../assets/erREST_40_1.jpg'
 import POOL1 from '../assets/pool2.jpg'
-const card = {
-  height: '100%',
-  padding: 0,
-  backgroundColor: 'rgba(17,0,0, 0.3)'
+const styles = {
+  card: {height: '100%', padding: 0, backgroundColor: 'rgba(17,0,0, 0.3)'},
+  img: {diplay: 'block', width: '100%', height: 'auto'},
+  col: {marginBottom: 20}
 }
-const img = {
-  // "height: 200px; width: 100%; display: block;"
-  diplay: 'block',
-  width: '100%',
-  height: 'auto'
-}
-// const row = { padding: 10}
+
 export default props => (
   <div className="container">
     <div className="row">
 
-      <div className="col-sm">
-        <div className="card" style={card}>
-            <img style={img} src={REST1} />
+      <div className="col-sm"  style={styles.col}>
+        <div className="card" style={styles.card}>
+            <img style={styles.img} src={POOL3} />
           <div className="card-body">
           </div>
 
         </div>
       </div>
       
-      <div className="col-sm">
-        <div className="card" style={card}>
-        <img style={img} src={REST2} />
+      <div className="col-sm"  style={styles.col}>
+        <div className="card" style={styles.card}>
+        <img style={styles.img} src={REST2} />
         </div>
       </div>
 
-      <div className="col-sm">
-        <div className="card" style={card}>
-        <img style={img} src={POOL1} />
+      <div className="col-sm"  style={styles.col}>
+        <div className="card" style={styles.card}>
+        <img style={styles.img} src={POOL1} />
 
         </div>
       </div>
